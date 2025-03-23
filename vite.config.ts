d5b15c7401b0309d498714567b04a8882c565f47
Fikih Sulaiman Pratama <75234603+fikih-hyperfanny@users.noreply.github.com>
2025-03-23 09:51:36 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,34 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  export default defineConfig({
+  base: "/https://fikih-hyperfanny.github.io/ciptarasa/", // Ganti "ciptarasa" sesuai dengan nama repositori GitHub kamu
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    themePlugin(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+        ]
+      : []),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+    },
+  },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
+});
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
